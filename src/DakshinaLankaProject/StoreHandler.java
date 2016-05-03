f5c0f4c944d3cc956ce9f6963ac160ce70ec1019
HP <HP@10.10.34.75>
2016-05-03 11:22:44 +0530

    pushed by vibodha 3/5/19

--- src/DakshinaLankaProject/StoreHandler.java
+++ src/DakshinaLankaProject/StoreHandler.java
@@ -5,23 +5,32 @@ import java.util.*;
 public class StoreHandler
 {
 	private Stock stock;
-	private ArrayList<ItemCategory> itemCategoryList;
+        private StoreDB storeDB;
+	
 
 	public final void removeFromStore(int itemID)
 	{
 		
 	}
 
-	public final void addToCategoryList(int item_id, Supplier supplier, String manufacturer, float buying_price, float selling_price, float amount)
+	public final void addFramesToList(String item_id, Supplier supplier, String manufacturer, float buying_price, float selling_price, int amount, String showroom_id,String frame_name,String frame_type)
 	{
-		
+            Frame frame = new Frame(item_id, buying_price, selling_price, supplier, manufacturer,amount, "", frame_name, frame_type);
+            stock.addToItemList(frame);
+            stock.addToTotal(buying_price);
 	}
 
-	public final void addToStock(Supplier supplier, ArrayList<ItemCategory> itemCategoryList)
+	public void setStock(Supplier supplier,int id, String date)
 	{
-		
+            stock.setSupplier(supplier);
+            stock.setStock_ID(id);
+            stock.setDate(date);
 	}
-
+        
+        public void addStock(Stock stock){
+            storeDB.addToStock(stock);
+        }
+        
 	public final void updateStock(Stock stock)
 	{
 		
